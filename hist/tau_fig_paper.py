640fd551fe9e0c06a3adce9dae9c2f2a56f5620e
Paulo Montero Camacho <pmontero@pcl.ac.cn>
2024-05-28 08:57:11 +0800

    Adding de Belsunce et al. (2021) to Fig 1

--- hist/tau_fig_paper.py
+++ hist/tau_fig_paper.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 
 """
 
-tau = np.array([0.051, 0.054, 0.054, 0.058, 0.059, 0.066, 0.069, 0.080])
-tau_e = [0.001, 0.002, 0.007, 0.006, 0.006, 0.013, 0.011, 0.012]
-y = [1, 2, 3, 4, 5, 6, 7, 8]
-markers = ['|', 'x', 'd', '8', 'p', 's', 'o', '^']
-labels = ['This work', 'Paoletti et al. 2024', 'Planck et al. 2020', 'Tristram et al. 2023', 'Pagano et al. 2020', 'Paradiso et al. 2023', 'Natale et al. 2020', r'Giar\`e et al. 2023']
-colors = ['green', 'gray', 'darkblue', 'orange', 'purple', 'brown', 'pink', 'magenta']
+tau = np.array([0.051, 0.054, 0.054, 0.058, 0.059, 0.063, 0.066, 0.069, 0.080])
+tau_e = [0.001, 0.002, 0.007, 0.006, 0.006, 0.005, 0.013, 0.011, 0.012]
+# for illustration purposes the asymetric error of Belsunce (^0.005 _0.0058)
+# is not crucial, let's average down
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+markers = ['|', 'x', 'd', '8', 'p', 'H', 's', 'o', '^']
+labels = ['This work', 'Paoletti et al. 2024', 'Planck et al. 2020', 'Tristram et al. 2023', 'Pagano et al. 2020', 'de Belsunce et al. 2021', 'Paradiso et al. 2023', 'Natale et al. 2020', r'Giar\`e et al. 2023']
+colors = ['green', 'gray', 'darkblue', 'orange', 'purple', 'cyan', 'brown', 'pink', 'magenta']
 
 plt.style.use('../5par.mplstyle')
 fig = plt.figure(figsize=(2,2))
