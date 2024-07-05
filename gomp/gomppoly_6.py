4bb0776aae7d4b4042153468325ed66104958b97
Yin Li <eelregit@gmail.com>
2024-07-05 12:09:23 +0800

    Merge likelihood with modifications into gomppoly impl

--- gomp/gomppoly_6.py
+++ gomp/gomppoly_6.py
@@ -1,6 +1,10 @@
 import numpy as np
 from numpy import exp, log
 from numpy.polynomial import Polynomial
+try:
+    from cobaya.likelihood import Likelihood
+except:
+    pass
 
 
 def reparam(h, omega_b, omega_cdm):
@@ -28,36 +32,53 @@ def gomppoly6(lna, lna_pivot=None, tilt=None, params=None):
     return exp(- exp(poly6(lna, lna_pivot=lna_pivot, tilt=tilt, params=params)))
 
 
-def pivot_sr(h, omega_b, omega_cdm, sigma8, n_s, zt, fit,
+def pivot_sr(h, omega_b, omega_cdm, sigma8, n_s, zt, sr_label,
              **kwargs):  # kwargs absorbs other params of Cobaya
     Ob, Om = reparam(h, omega_b, omega_cdm)
     s8, ns = sigma8, n_s
 
-    if fit == 'gomp1':  # complexity 22
+    if sr_label == 'gomp1':  # complexity 22
         return (ns + 0.35580978 * log(0.11230898 * zt)) * (0.048352774 - s8) - Om - ns + (Ob / Om) ** h
-    if fit == 'gomp1_raw':
+    if sr_label == 'gomp1_raw':
         return ((((ns - (log(0.11230898 * zt) * -0.35580978)) * (0.048352774 - s8)) - (Om + ns)) + ((Ob / Om) ** h))
 
-    #if fit == 'gomp2':  # complexity ??
-    #if fit == 'gomp2_raw':
+    #if sr_label == 'gomp2':  # complexity ??
+    #if sr_label == 'gomp2_raw':
 
-    raise ValueError(f'fit = {fit} not recognized')
+    raise ValueError(f'sr_label = {sr_label} not recognized')
 
 
-def tilt_sr(h, omega_b, omega_cdm, sigma8, n_s, zt, fit,
+def tilt_sr(h, omega_b, omega_cdm, sigma8, n_s, zt, sr_label,
             **kwargs):  # kwargs absorbs other params of Cobaya
     Ob, Om = reparam(h, omega_b, omega_cdm)
     s8, ns = sigma8, n_s
 
-    if fit == 'gomp1':  # complexity 25
+    if sr_label == 'gomp1':  # complexity 25
         return log(Ob) * (0.005659511 ** Om / 0.601493 - log(zt - (Om + ns * h) ** 15.051933) + h) + h / s8
-    if fit == 'gomp1_raw':
+    if sr_label == 'gomp1_raw':
         return ((log(Ob) * (((0.005659511 ** Om) / 0.601493) - (log(zt - ((Om + (ns * h)) ** 15.051933)) - h))) + (h / s8))
 
-    #if fit == 'gomp2':  # complexity ??
-    #if fit == 'gomp2_raw':
+    #if sr_label == 'gomp2':  # complexity ??
+    #if sr_label == 'gomp2_raw':
 
-    raise ValueError(f'fit = {fit} not recognized')
+    raise ValueError(f'sr_label = {sr_label} not recognized')
+
+
+class CombinedQuasar(Likelihood):
+
+    def initialize(self):
+        self.lna = - np.log(1 + 7.29)
+        self.mean = 0.49
+        self.var = 0.11 ** 2  # symmetric
+
+    def get_requirements(self):
+        return dict(H0=None)
+
+    def logp(self, **param_values):
+        param_values['h'] = self.provider.get_param('H0') / 100
+        param_values['sr_label'] = 'gomp1'
+        xHI = gomppoly6(self.lna, params=param_values)
+        return -0.5 * (np.log(2 * np.pi * self.var) + (xHI - self.mean) ** 2 / self.var)
 
 
 if __name__ == '__main__':
@@ -69,14 +90,14 @@ if __name__ == '__main__':
         n_s=0.9660499,
         zt=24,
     )
-    #fits = ['gomp1', 'gomp1_raw']
-    fits = ['gomp1']
+    #sr_labels = ['gomp1', 'gomp1_raw']
+    sr_labels = ['gomp1']
 
     import matplotlib.pyplot as plt
     z = np.linspace(5, 16, num=111, endpoint=True)
-    for fit in fits:
-        params['fit'] = fit
-        plt.plot(z, gomppoly6(log(1/(1+z)), params=params), label=fit, lw=1, alpha=.5)
+    for sr_label in sr_labels:
+        params['sr_label'] = sr_label
+        plt.plot(z, gomppoly6(log(1/(1+z)), params=params), label=sr_label, lw=1, alpha=.5)
     plt.legend()
     plt.xlabel('$z$')
     plt.ylabel('$x_\mathrm{HI}$')
