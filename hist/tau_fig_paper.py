52bebdf4d6e381dc7d25c0639f1d5932fab3ba9f
Paulo Montero Camacho <pmontero@pcl.ac.cn>
2024-07-10 15:56:22 +0800

    Finally added Qin et al to tau fig! + edits

--- hist/tau_fig_paper.py
+++ hist/tau_fig_paper.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 
 """
 
-taus = [.053, .054, .054, .058, .059, .0619, .063, .066, .069, .080]
-errors = [.001, .002, .007, .006, .006, ([.0068], [.0056]), ([.0058], [.005]), .013, .011, .012]
-ys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-markers = ['|', 'x', 'd', '8', 'p', '*', 'H', 's', 'o', '^']
-labels = ['This work', 'Paoletti et al. 2024', 'Planck et al. 2020',
+taus = [.053, .054, .054, 0.057, .058, .059, .0619, .063, .066, .069, .080]
+errors = [.001, .002, .007, ([0.0066], [0.0081]), .006, .006, ([.0068], [.0056]), ([.0058], [.005]), .013, .011, .012]
+ys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+markers = ['|', 'x', 'd', 'h', '8', 'p', '*', 'H', 's', 'o', '^']
+labels = ['This work', 'Paoletti et al. 2024', 'Planck et al. 2020', 'Qin et al. 2020',
           'Tristram et al. 2023', 'Pagano et al. 2020', 'Heinrich \& Hu 2021',
           'de Belsunce et al. 2021', 'Paradiso et al. 2023', 'Natale et al. 2020',
           'Giar\`e et al. 2023']
-colors = ['C2', 'C7', 'C0', 'C1', 'C3', 'C4', 'C9', 'C5', 'C6', 'C8']
+colors = ['C2', 'C7', 'C0', 'purple', 'C1', 'C3', 'C4', 'C9', 'C5', 'C6', 'C8']
 
 plt.style.use('../5par.mplstyle')
 fig, ax = plt.subplots(figsize=(2.7, 2.7))
