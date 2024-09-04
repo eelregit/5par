652ba761c60b3bea61ed83400cd9c2e759be1306
Yin Li <eelregit@gmail.com>
2024-09-04 20:49:54 +0800

    Edit thesan plot

--- gomp/thesan_fit.py
+++ gomp/thesan_fit.py
@@ -1,7 +1,4 @@
-from functools import partial
-
 import numpy as np
-from numpy.polynomial import Polynomial
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -10,15 +7,9 @@ from gomppoly_6 import poly6, gomppoly6
 
 # THESAN reionization history is available at
 # https://www.thesan-project.com/quantities/reion_history_Thesan1.dat
-
 z, x = np.loadtxt('reion_history_Thesan1.dat', unpack=True)[:2]
-
 a = 1 / (1 + z)
 lna = np.log(a)
-#
-#
-#def gomppoly6_(lna, lna_pivot, tilt):
-#    return gomppoly6(lna, lna_pivot=lna_pivot, tilt=tilt, params=None)
 
 
 def fit(lna, x):
@@ -32,11 +23,14 @@ def fit(lna, x):
 
 
 def plot(lna, lna_pivot, tilt, x):
+    a = np.exp(lna)
     a_ = np.geomspace(1e-2, 1, num=101)  # for analytic curves
 
     plt.style.use('../5par.mplstyle')
     fig, axes = plt.subplots(nrows=2, sharex=True,
-                             gridspec_kw={'wspace': 0, 'hspace': 0}, figsize=(4, 5))
+                             gridspec_kw={'height_ratios': [2, 1], 'wspace': 0,
+                                          'hspace': 0},
+                             figsize=(4, 4))
 
     axes[0].plot(a, x, c='gray', label='THESAN simulation')
     axes[0].plot(a_, gomppoly6(np.log(a_), lna_pivot, tilt), c='C0', ls='--',
@@ -45,10 +39,10 @@ def plot(lna, lna_pivot, tilt, x):
     axes[0].set_ylim(-0.1, 1.1)
     axes[0].legend()
 
-    axes[1].plot(a, np.log(-np.log(x)), c='gray')
-    axes[1].plot(a_, poly6(np.log(a_), lna_pivot, tilt), c='C0', ls='--')
-    axes[1].set_ylabel(r'$\ln(-\ln x_\mathrm{HI})$')
-    axes[1].set_ylim(-9, 4)
+    axes[1].axhline(0, c='gray')
+    axes[1].plot(a, gomppoly6(lna, lna_pivot, tilt) - x, c='C0', ls='--')
+    axes[1].set_ylabel('abs. diff.')
+    axes[1].set_ylim(-0.015, 0.015)
     axes[1].set_xlabel(r'$a$')
     axes[1].set_xscale('log')
     axes[1].set_xlim(a.min(), a.max())
