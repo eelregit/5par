767333f5727968068bb51519ac89f8d3fd26c7ca
Yin Li <eelregit@gmail.com>
2025-04-27 13:53:22 +0800

    Add resuming, time limit, and mem limit to MCMC jobs

--- robust_gomp/mcmc/cmb_dw_lens_w0waMnu/job.sh
+++ robust_gomp/mcmc/cmb_dw_lens_w0waMnu/job.sh
@@ -3,6 +3,8 @@
 #SBATCH --job-name=cobaya
 #SBATCH --output=%x-%j.out
 #SBATCH --partition=deimos
+#SBATCH --mem-per-cpu=7200M
+#SBATCH --time=7-00:00:00
 
 
 export OMP_NUM_THREADS=4
@@ -12,6 +14,6 @@ source $HOME/Projects/gomp/activate.sh
 
 hostname; pwd; date
 
-time mpirun -n 16 -ppn 4 cobaya run cobaya.yaml
+time mpirun -n 16 -ppn 4 cobaya run --resume cobaya.yaml
 
 date
